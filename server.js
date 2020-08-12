aa78d7c7ae9e27f660f2d89f1a5eb14333817a56
Justin <ponthierjustin@gmail.com>
2020-08-12 14:37:27 -0400

    adding route for deployment

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",{
   useNewUrlParser: true,
   useUnifiedTopology: true,
