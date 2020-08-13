8ead3dc310dd630317dd046758527c66fd120567
Justin <ponthierjustin@gmail.com>
2020-08-13 09:28:56 -0400

    fixing paths again

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const path = require("path");
 const app = express();
+const routes = require("./routes")
 const PORT = process.env.PORT || 3001;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
-
-
+};
 app.use(routes);
-
+app.get("/api/config", (req, res) => {
+  res.json({ success: true });
+});
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-app.listen(PORT, function () {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/gt-react-google-books", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to database.");
+  })
+  .catch(err => {
+    console.log("Unable to connect to database.");
+    console.log(err);
+  });
+app.listen(PORT, () => {
+  console.log(`Express server is running on http://localhost:${PORT}`);
 });
